f81b993f424bc03d7fbebf391512875c6a07e449
jkrrahul2608 <67185193+jkrrahul2608@users.noreply.github.com>
2023-09-07 07:42:31 +0000

    Data types done

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,21 @@
+"use strict"; // to let compiler know that use new javascript
+
+//alert(3 +3) //it won't work because we use node.js not browser
+
+let name = "Rahul"
+let age = 28
+let isLoggedIn = true
+
+
+//number
+//bigInt
+//string = ""
+//boolean = true/false
+//null = standlone value
+// undefined =>
+// symbol => unique = > used in react to have unique components
+
+//object
+console.log(typeof age)
+//null type is Object
+//undefined type is undefined
