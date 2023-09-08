87bd0a00a65222360cc8c2659b5d1987a4d4fb6e
jkrrahul2608 <67185193+jkrrahul2608@users.noreply.github.com>
2023-09-08 14:28:26 +0000

    Conversion done

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,20 @@
+// //let myString = "33a" //Nan
+// let myString = null
+// let myString = undefined // Nan
+// true => 1, false => 0
+
+// console.log(typeof myString)
+// let ValueInNumber = Number(myString)
+// console.log(typeof ValueInNumber)
+// console.log(ValueInNumber)
+
+//let isLoggedIn = 0 // only 0 becomes false, -ve and +ve numbers becomes true
+// let isLoggedIn = " " //"" is false, " " becomes true and "Rahul" becomes true
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn)
+
+let myNumber = 33
+let myString = String(myNumber)
+console.log(typeof myString);
+
+
